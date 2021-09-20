b276b1ac8692e5ea17f95ce88e42b91bca2aac1e
ramashankar kumar <ramashankar.kumar@gridinfocom.com>
2021-09-20 18:31:24 +0530

    Day16

--- Day16/src/main/java/com/example/demo/Day16Application.java
+++ Day16/src/main/java/com/example/demo/Day16Application.java
@@ -1,12 +1,17 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Day16Application {
+	private static final Logger logger = LoggerFactory.getLogger(Day16Application.class);
 
 	public static void main(String[] args) {
+
+		logger.info("This is a info message");
 		SpringApplication.run(Day16Application.class, args);
 	}
 
